7f1ef67586e702da9549d8c78f1da07b3f9cf3c0
ParkYoungWoong <thesecon@gmail.com>
2017-12-23 12:19:15 +0900

    Start React.js

---
+++ src/js/components/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    let name = 'HEROPY';
+    return (
+      <div>
+        <h2>Hello {name}</h2>
+      </div>
+    )
+  }
+}
+
+export default Hello;
